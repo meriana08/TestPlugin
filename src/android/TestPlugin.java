c61288f73aaea9d9bf4fa1957bc5c9c722ae0406
meriana08 <31026295+meriana08@users.noreply.github.com>
2018-05-03 13:44:03 +0700

    Create TestPlugin.java

---
+++ src/android/TestPlugin.java
@@ -0,0 +1,41 @@
+package com.meriana08.cordova.plugin;
+// The native Toast API
+import android.widget.Toast;
+// Cordova-required packages
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+public class TestPlugin extends CordovaPlugin {
+  private static final String DURATION_LONG = "long";
+  @Override
+  public boolean execute(String action, JSONArray args,
+    final CallbackContext callbackContext) {
+      // Verify that the user sent a 'show' action
+      if (!action.equals("show")) {
+        callbackContext.error("\"" + action + "\" is not a recognized action.");
+        return false;
+      }
+      String message;
+      String duration;
+      try {
+        JSONObject options = args.getJSONObject(0);
+        message = options.getString("message");
+        duration = options.getString("duration");
+      } catch (JSONException e) {
+        callbackContext.error("Error encountered: " + e.getMessage());
+        return false;
+      }
+      // Create the toast
+      Toast test = Test.makeText(cordova.getActivity(), message,
+        DURATION_LONG.equals(duration) ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT);
+      // Display toast
+      test.show();
+      // Send a positive result to the callbackContext
+      PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
+      callbackContext.sendPluginResult(pluginResult);
+      return true;
+  }
+}
